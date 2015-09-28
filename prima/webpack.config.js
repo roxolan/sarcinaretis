c289c0a6d179740aa34adf98eaf9d3c6461bef4e
roxolan <roxolanus@gmail.com>
2015-09-28 18:54:58 +0300

    refactoring files to a more realistic folder structure, enabling virtual build subfolder, making configs to serve files from specific directories

--- prima/webpack.config.js
+++ prima/webpack.config.js
@@ -1,8 +1,18 @@
+var path = require('path');
+
 module.exports = {
+  context: path.resolve('js'),
   entry: ["./util", "./app"],
   output: {
+    path: path.resolve('build/js/'),
+    publicPath: '/public/assets/js/',
     filename: "bundle.js"
   },
+
+  devServer: {
+    contentBase: 'public'
+  },
+
   watch: true,
 
   module: {
